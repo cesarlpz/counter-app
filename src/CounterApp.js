328415b5adf715653dc168b844de6da713273c8c
Cesar Lopez <ce.lopez.dj@gmail.com>
2021-12-27 17:31:27 -0700

    added CounterApp.js

--- src/CounterApp.js
+++ src/CounterApp.js
@@ -2,17 +2,22 @@ import React from "react";
 import PropTypes from "prop-types";
 
 const CounterApp = ({ value }) => {
+
+    const handleAdd = (e) => {
+        console.log(e);
+    }
+
     return <>
         <h1>CounterApp</h1>
         <h2>{ value }</h2>
 
-        <button onClick={ () => {console.log("onClick fired") }}>+1</button>
+        <button onClick={ handleAdd }>+1</button>
     </>
+}
 
-    CounterApp.propTypes = {
-        value: PropTypes.number
-    }
+CounterApp.propTypes = {
+    value: PropTypes.number
 }
 
 //Exports functional component.
-export default CounterApp; 
+export default CounterApp;
