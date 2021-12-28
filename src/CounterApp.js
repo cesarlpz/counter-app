4ab253582029b5c345fd0d54b46bef3ba03f9ba9
Cesar Lopez <ce.lopez.dj@gmail.com>
2021-12-27 18:17:42 -0700

    Created buttons substract and reset, added their corresponding handle events, gave different classNames to handle with properly on css file

--- src/CounterApp.js
+++ src/CounterApp.js
@@ -1,21 +1,31 @@
 import React, { useState } from "react";
 import PropTypes from "prop-types";
 
-const CounterApp = ({ value }) => {
-    const [counter, setCounter] = useState(0);
+const CounterApp = ({ value = 10 }) => {
+    const [counter, setCounter] = useState( value );
 
-    const handleAdd = (e) => {
+    const handleAdd = () => {
         setCounter(counter + 1);
         
         //Setting state val with function
         //setCounter((c) => c + 1);
     }
 
+    const handleSubstract = () => {
+        setCounter(counter - 1);
+    }
+
+    const handleReset = () => {
+        setCounter(value);
+    }
+
     return <>
         <h1>CounterApp</h1>
         <h2>{ counter }</h2>
 
-        <button onClick={ handleAdd }>+1</button>
+        <button className="numericButton" onClick={ handleAdd }>+1</button>
+        <button className="textButton" onClick={ handleReset }>Reset</button>
+        <button className="numericButton" onClick={ handleSubstract }>-1</button>
     </>
 }
 
